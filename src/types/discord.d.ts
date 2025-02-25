ea2f187cf02832cae12f2dad394c85b9e25c7adb
Kukuri and the Harmonic Odyssey <83948007+Mikasuru@users.noreply.github.com>
2025-02-25 14:23:27 +0700

    Delete src directory

--- src/types/discord.d.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from 'discord.js-selfbot-v13';
-import { CommandHandler } from '../modules/CommandHandler';
-
-declare module 'discord.js-selfbot-v13' {
-    interface Client {
-        commandHandler?: CommandHandler;
-    }
-}
